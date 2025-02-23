c83d313691c9625c30264f1b0a10d1ec75eb71e4
jay-dotmp3 <testerforrbx@gmail.com>
2025-02-23 07:05:17 +0100

    pmo

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
 function gamejolt(){
     window.location.href = 'https://www.gamejolt.com/jaydotmp3';
 }
+
+var butt = document.getElementsByClassName("button");
+var buttprop = document.getBoundingClientRect();
+
+function button_hover(){
+    
+    console.log("move");
+}

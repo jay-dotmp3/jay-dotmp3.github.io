034889214470df2c5107362632fe0d094f408495
jay-dotmp3 <testerforrbx@gmail.com>
2025-02-24 05:29:15 +0100

    whatever

--- home.js
+++ home.js
@@ -1,11 +1,3 @@
 function gamejolt(){
-    window.location.href = 'https://www.gamejolt.com/jaydotmp3';
-}
-
-var butt = document.getElementsByClassName("button");
-var buttprop = document.getBoundingClientRect();
-
-function button_hover(){
-    
-    console.log("move");
+    window.location.href = 'https://www.gamejolt.com/@jaydotmp3';
 }

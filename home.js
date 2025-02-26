acc3a5c4b248f69d203c24e4d29dcbdea15832fb
jay-dotmp3 <testerforrbx@gmail.com>
2025-02-26 06:35:25 +0100

    port to flexbox

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
-function gamejolt(){
-    window.location.href = 'https://www.gamejolt.com/@jaydotmp3';
+function yt(){
+    window.open('https://www.youtube.com/@jaydotmp3', '_blank').focus();
+}
+
+function gj(){
+    window.open('https://www.gamejolt.com/@jaydotmp3', '_blank').focus();
+}
+
+function x(){
+    window.open('https://www.x.com/jaydotmp3', '_blank').focus();
 }

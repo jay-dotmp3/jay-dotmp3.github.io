db4ac02210425fb64b1dacc294c7749fa3289488
jay-dotmp3 <testerforrbx@gmail.com>
2025-03-01 13:13:34 +0100

    i mightve figured it out sigma

--- home.js
+++ home.js
@@ -9,3 +9,20 @@ function gj(){
 function x(){
     window.open('https://www.x.com/jaydotmp3', '_blank').focus();
 }
+
+var dictionary = [
+    {
+        site:"yt",
+        link:"https://www.youtube.com/@jaydotmp3"
+    },
+    
+    {
+        site:"gj",
+        link:"https://www.gamejolt.com/@jaydotmp3"
+    },
+
+    {
+        site:"x",
+        link:"https://www.x.com/jaydotmp3"
+    }
+]
